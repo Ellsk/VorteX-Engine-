38b30b9fc5edf5190df5a97062a87a32b9cfdfc0
Elliot Kumedzro <ebkumedzro@st.ug.edu.gh>
2023-09-19 19:33:41 +0100

    Created a static class and UI

---
+++ VortexEditor.0/GameProject/NewProject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VortexEditor._0.GameProject
+{
+    class NewProject : ViewModelBase
+    {
+        private string _name = "NewProject";
+
+        public string Name
+        {
+            get => _name;
+
+            set
+            {
+                if (_name != value)
+                {
+                    _name = value;
+                    OnPropertyChanged(nameof(Name));
+                }
+            }
+        }
+
+    }
+}
+    
+
